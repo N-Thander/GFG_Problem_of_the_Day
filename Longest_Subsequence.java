2d77634f235388fd1b76b1406a429e31befae86e
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-05-27 09:19:39 +0530

    Create Longest_Subsequence.java

---
+++ Longest_Subsequence.java
@@ -0,0 +1,31 @@
+
+class Solution {
+    public static int longestSubseq(int n, int[] a) 
+    {
+        int [] dp =  new int [n];
+        
+        for (int i = 0; i < n; i++)
+        {
+            dp[i] = 1;
+        }
+        
+        for(int i = 1; i < n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if (Math.abs(a[i] - a[j]) == 1)
+                {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+        }
+        
+        int maxLength = 0;
+        for(int i = 0; i < n; i++)
+        {
+            maxLength = Math.max(maxLength, dp[i]);
+        }
+        
+        return maxLength;
+    }
+}
