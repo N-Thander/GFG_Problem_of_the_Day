8f92728421bf76147e6dfbae755e4a17d204624d
N-Thander <108251321+N-Thander@users.noreply.github.com>
2024-05-31 12:01:44 +0530

    Create Swap_two_nibble_in_byte.java

---
+++ Swap_two_nibble_in_byte.java
@@ -0,0 +1,10 @@
+
+// User function Template for Java
+class Solution {
+    static int swapNibbles(int n) 
+    {
+        int rn = (n & 0xF0) >> 4; 
+        int ln = (n & 0x0F) << 4; 
+        return rn | ln;
+    }
+}
